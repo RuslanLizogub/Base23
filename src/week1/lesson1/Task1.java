cf8053cd880016f158155c538a11858b73ab96ae
ruslan_lizogub <ruslanlizogub0201@gmail.com>
2015-04-27 11:00:48 +0300

    Test commit2

--- src/week1/lesson1/Task1.java
+++ src/week1/lesson1/Task1.java
@@ -4,5 +4,6 @@ public class Task1 {
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 		System.out.println("Test commit");
+		System.out.println("Test commit2");
 	}
 }
