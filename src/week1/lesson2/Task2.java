316a653464f0ff7087a98a5327ac0b44c8b790f0
ruslan_lizogub <ruslanlizogub0201@gmail.com>
2015-04-29 17:41:02 +0300

    week1.lesson2 task2

--- src/week1/lesson2/Task2.java
+++ src/week1/lesson2/Task2.java
@@ -4,12 +4,21 @@ package week1.lesson2;
  */
 public class Task2 {
 	public static void main(String[] args){
-		String string1 = "ababab";
-		String string2 = "ab";
+		String string1 = "fffffffffffffabababab";
+		String string2 = "fab";
 		
 		countRepeat(string1,string2);
 	}
 	public static void countRepeat(String string1,String string2){
-		
+		int lengthString1 = string1.length();
+		int lengthString2 = string2.length();
+
+		int count = 0;
+		for(int i = 0; i < lengthString1; i++){
+			if(string1.indexOf(string2,i) == i){
+				count = count + 1;
+			}
+		}
+		System.out.println(count);
 	}
 }
