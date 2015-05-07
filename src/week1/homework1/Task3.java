ebc1d2963389802bc9222f1c53c1dfddb72ec21d
ruslan_lizogub <ruslanlizogub0201@gmail.com>
2015-05-07 16:39:30 +0300

    week1.homework1.Task3

--- src/week1/homework1/Task3.java
+++ src/week1/homework1/Task3.java
@@ -8,5 +8,44 @@ package week1.homework1;
  true
  */
 public class Task3 {
-
+	public static void main(String[] args){
+		int boxWidth = 10; 
+		int boxHeight = 5; 
+		int shelfWidth = 5; 
+		int shelfHeight = 10;
+		
+		if(isContains(boxWidth,boxHeight,shelfWidth,shelfHeight) == true){
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}
+	}
+	
+	public static boolean isContains(int boxWidth, int boxHeight, int shelfWidth, int shelfHeight){
+		boolean bufer;
+		boolean bufer2;
+		boolean result;
+		
+		if(boxWidth <= shelfWidth || boxWidth <= shelfHeight){
+			bufer = true;
+		}else{
+			bufer = false;
+		}
+		
+		if(boxHeight <= shelfHeight || boxHeight <= shelfWidth){
+			bufer2 = true;
+		}else{
+			bufer2 = false;
+		}
+		
+		
+		if(bufer == true && bufer2 == true){
+			result = true;
+		}else{
+			result = false;
+		}
+		
+		return result;
+	}
+	
 }
