f33098e02b465471b98de5ba1d1c58df2dcbee3e
ruslan_lizogub <ruslanlizogub0201@gmail.com>
2015-05-07 16:01:09 +0300

    week1.homework1.task2

--- src/week1/homework1/Task2.java
+++ src/week1/homework1/Task2.java
@@ -1,4 +1,7 @@
 package week1.homework1;
+
+import java.util.Scanner;
+
 /*
  * Пользователь вводит высоту и ширину двумерного массива (матрицы). Заполнить матрицу случайными числами [0..9] и вывести на экран.
  При вводе неверных размеров массива, выдавать сообщение "Не верный размер массива" и выходить из программы.
@@ -11,5 +14,40 @@ package week1.homework1;
  9123
  */
 public class Task2 {
-
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	    int heihgt = scan.nextInt();
+		int width = scan.nextInt();
+		
+		twoDimensionalArray(heihgt,width);
+	}
+	
+	public static void twoDimensionalArray(int heihgt,int width){
+		int[][] mayArray;
+		
+		if(heihgt <= 0 || width <= 0){
+			System.out.println("Не верный размер массива");
+		}
+		
+		mayArray = new int[heihgt][width];
+		
+		//заполняем масив случайными числами
+		for(int i = 0;i < heihgt; i++){
+			for(int j = 0;j < width; j++){
+				if(j == 0){
+					mayArray[i][j] =  (int) (Math.random()*9);	
+				}else{
+					mayArray[i][j] =  (int) (Math.random()*9);
+				}
+			} 
+		}
+		
+		//выводим на екран результат
+		for(int i = 0; i < heihgt; i++){
+			for(int j = 0; j < width; j++){
+				System.out.print(mayArray[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
 }
