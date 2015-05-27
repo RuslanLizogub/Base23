55ecc034259ffbab3422fe66572b36857b3e9a37
ruslan_lizogub <ruslanlizogub0201@gmail.com>
2015-05-27 18:38:34 +0300

    LinkList начало)

--- src/week4/homework4/LinkList.java
+++ src/week4/homework4/LinkList.java
@@ -1,4 +1,5 @@
 package week4.homework4;
+
 /*
  * Написать класс LinkList, содержащий элементы типа int, реалзизующем связанный список с классом элемента Node.
  Для класса LinkList написать методы:
@@ -16,9 +17,149 @@ package week4.homework4;
  11*. Удалить повторяющиеся элементы из списка, в случае если реализован интерфейс Comparable, метод void removeDuplicates()
  */
 public class LinkList {
+
 	public static void main(String[] args) {
-		LinkList myList = new LinkList();
-		
+		LinkList list = new LinkList();
+		for (int i = 0; i < 5; i++) {
+			list.addElementFront(i, i + 1);
+		}
+
+		set(1, 222);
+		printList();
+		System.out.println(indexOf(222));
+	}
+	/////////////
+	public static int indexOf(int element){
+		LinkList t = head;
+		while (t.next != null) {
+			if (t.next.element == element) {
+				return t.next.index;
+			}
+			t = t.next;
+		}
+		return -1;
+	}
+	
+	public static void remove(int index){
+		if (head == null) {
+			return;
+		}
+
+		if (head == tail) {
+			head = null;
+			tail = null;
+			return;
+		}
+
+		if (head.index == index) {
+			head = head.next;
+			return;
+		}
+
+		LinkList t = head;
+		while (t.next != null) {
+			if (t.next.index == index) {
+				if (tail == t.next) {
+					tail = t;
+				}
+				t.next = t.next.next;
+				return;
+			}
+			t = t.next;
+		}
+	}
+	
+	public static void set(int index, int element){
+		LinkList t = head;
+		while (t.next != null) {
+			if (t.next.index == index) {
+				t.next.element = element;
+				return;
+			}
+			t = t.next;
+		}
+	}
+
+	// //////////
+	static LinkList head;
+	static LinkList tail;
+
+	public void addElementFront(int index, int element) { // добавляет елемент
+															// спереди
+		LinkList a = new LinkList(index, element);
+
+		if (head == null) {
+			head = a;
+			tail = a;
+		} else {
+			a.next = head;
+			head = a;
+		}
+	}
+
+	public void addElementBack(int index, int element) { // добавляет елемент в
+															// конец
+		LinkList a = new LinkList(index, element);
+		if (tail == null) {
+			head = a;
+			tail = a;
+		} else {
+			tail.next = a;
+			tail = a;
+		}
+	}
+
+	static void printList() {
+		LinkList t = head;
+		while (t != null) {
+			System.out.println("Element = " + t.element + ", Index = "
+					+ t.index + ".");
+			t = t.next;
+		}
+	}
+
+	void delElement(int index) {
+		if (head == null) {
+			return;
+		}
+
+		if (head == tail) {
+			head = null;
+			tail = null;
+			return;
+		}
+
+		if (head.index == index) {
+			head = head.next;
+			return;
+		}
+
+		LinkList t = head;
+		while (t.next != null) {
+			if (t.next.index == index) {
+				if (tail == t.next) {
+					tail = t;
+				}
+				t.next = t.next.next;
+				return;
+			}
+			t = t.next;
+		}
+
+	}
+
+	// //////////
+	int index;
+	int element;
+	LinkList next;
+
+	public LinkList(int index, int element) {
+		this.index = index;
+		this.element = element;
+	}
+
+	public LinkList() {
+
 	}
 
 }
